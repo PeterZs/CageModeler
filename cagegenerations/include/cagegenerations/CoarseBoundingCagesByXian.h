d5ca326cd3fbf719dfa167630fe2269054ae0a68
Arne815 <arne.98.lich@gmx.de>
2026-05-02 16:50:26 +0200

    voxel list comprehesion

--- cagegenerations/include/cagegenerations/CoarseBoundingCagesByXian.h
+++ cagegenerations/include/cagegenerations/CoarseBoundingCagesByXian.h
@@ -3,16 +3,6 @@
 #include <Eigen/Geometry>
 #include <vector>
 
-struct BBVoxel {
-    Eigen::Vector3d start_pt;
-    std::vector<std::vector<std::vector<Eigen::Vector3d>>> centers, voxel_pts;
-    int n_voxel[3];
-    float res_voxel[3];
-    std::vector<std::vector<std::vector<int8_t>>> voxel_types; // 0 = feature voxel, -1 = inner voxel, 1 = outer voxel
-    std::vector<int> non_manifold_vertices;
-    std::vector<Eigen::Vector3d> splitted_vertices;
-};
-
 struct BBVoxel_ {
     Eigen::Vector3d start_pt;
     std::vector<Eigen::Vector3d> centers_, voxel_pts_;
@@ -23,11 +13,6 @@ struct BBVoxel_ {
     std::vector<Eigen::Vector3d> splitted_vertices;
 
     int getVoxelId(int x, int y, int z)
-    {
-        return z + y * n_voxel[2] + x * n_voxel[2] * n_voxel[1];
-    }
-
-    int getCenterId(int x, int y, int z)
     {
         return z + y * n_voxel[2] + x * n_voxel[2] * n_voxel[1];
     }
@@ -43,7 +28,7 @@ void calculatePCA(const Eigen::MatrixXd& mesh_vertices, Eigen::MatrixXd& pca_bas
 
 void computeBB(const Eigen::MatrixXd& vertices, Eigen::MatrixXd& cage_vertices, Eigen::MatrixXi& cage_faces, const Eigen::MatrixXd& pca_basic_matrix, const Eigen::Vector3d& barycenter);
 
-void voxelizeBB(const Eigen::MatrixXd& mesh_vertices, const Eigen::MatrixXd& cage_vertices, BBVoxel& voxels, BBVoxel_& voxels_, float degreeOfSparseness);
+void voxelizeBB(const Eigen::MatrixXd& mesh_vertices, const Eigen::MatrixXd& cage_vertices, BBVoxel_& voxels_, float degreeOfSparseness);
 
 void minMax(const double v0, const double v1, const double v2, double& min, double& max);
 bool planeBoxOverlap(const Eigen::Vector3d normal, const double d, const Eigen::Vector3d maxbox);
@@ -53,18 +38,13 @@ bool axistest_z12(const double a, const double b, const double fa, const double 
 bool axistest_z0(const double a, const double b, const double fa, const double fb, const Eigen::Matrix3d& v, const Eigen::Vector3d& halfbox);
 bool axistest_x2(const double a, const double b, const double fa, const double fb, const Eigen::Matrix3d& v, const Eigen::Vector3d& halfbox);
 bool axistest_y1(const double a, const double b, const double fa, const double fb, const Eigen::Matrix3d& v, const Eigen::Vector3d& halfbox);
-void identifyFeatureVoxels(BBVoxel& voxels, BBVoxel_& voxels_, const Eigen::MatrixXd& mesh_vertices, const Eigen::MatrixXi& mesh_faces);
+void identifyFeatureVoxels(BBVoxel_& voxels_, const Eigen::MatrixXd& mesh_vertices, const Eigen::MatrixXi& mesh_faces);
 
-void identifyOuterVoxels(BBVoxel& voxels, BBVoxel_& voxels_);
-void identifyOuterVoxelsWithFillingAlgo(BBVoxel& voxels, const Eigen::Vector3i seed);
-////////////////////////
+void identifyOuterVoxels(BBVoxel_& voxels_);
+
 void identifyOuterVoxelsWithFillingAlgo(BBVoxel_& voxels_, const Eigen::Vector3i seed);
-///////////////////////
 
-void extractOuterSurface(BBVoxel& voxels, Eigen::MatrixXd& cage_vertices, Eigen::MatrixXi& cage_faces);
-///////////////////////////
 void extractOuterSurface(BBVoxel_& voxels_, Eigen::MatrixXd& cage_vertices, Eigen::MatrixXi& cage_faces);
-///////////////////////////
 
 void smoothCage(Eigen::MatrixXd& cage_vertices, Eigen::MatrixXi& cage_faces, float lambda_smooth, int it_smooth, Eigen::MatrixXd& pca_based_mesh_vertices, const Eigen::MatrixXi& mesh_faces);
 
